3c122d07e22f852e1ae28ca0f63c75e031ffb460
Praveenpraveen12 <146430281+Praveenpraveen12@users.noreply.github.com>
2025-03-14 17:07:26 +0530

    Create static_demo.java

---
+++ static_demo.java
@@ -0,0 +1,16 @@
+package com.variable;
+
+public class static_demo 
+{
+	static String name ="praveen";
+	static int age =17;
+	static double salary = 25000;
+	
+	
+	public static void main(String args[])
+	{
+		System.out.println("static variable name :" + static_demo.name);
+		System.out.println("static variable age :" + static_demo.age);
+		System.out.println("static variable salary :" + static_demo.salary);
+	}
+}
